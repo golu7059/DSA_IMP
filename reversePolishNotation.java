896ecf53f27ae25d75e0c3033f3dac1ccc063986
golu7059 <golukumarjaisidih@gmail.com>
2024-01-30 21:18:02 +0530

    leetcode 150

---
+++ reversePolishNotation.java
@@ -0,0 +1,29 @@
+//   leetcode 150 : reverse polish notation
+
+class Solution {
+    int pos;
+    public int evalRPN(String[] tokens) {
+        this.pos = tokens.length-1;
+        return evaluate(tokens);
+    }
+
+    public int evaluate(String[] t){
+        String token = t[pos];
+        pos--;
+        if(token.equals("+")){
+            return evaluate(t) + evaluate(t); 
+        }else
+        if(token.equals("*")){
+            return evaluate(t) * evaluate(t);
+        }else 
+        if(token.equals("-")){
+            return -evaluate(t) + evaluate(t);
+        }else
+        if(token.equals("/")){
+            int right = evaluate(t);
+            return evaluate(t) / right;
+        }else{
+            return Integer.parseInt(token);
+        }
+    }
+}
