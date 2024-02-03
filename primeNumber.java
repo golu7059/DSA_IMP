0a16d00586e37d451e7244957a68f1e5fa967992
golu7059 <golukumarjaisidih@gmail.com>
2024-02-04 00:35:06 +0530

    square root algorithm to find prime

--- primeNumber.java
+++ primeNumber.java
@@ -1,10 +1,21 @@
 public class primeNumber {
     public static void main(String[] args) {
-        int n = 100;
-        for(int j=1; j<=n ; j++){
-            for (int i=2; i< Math.pow(j, 0.5); i++){
-                if(j%i != 0) System.out.println(j);
-            }
+        int n = 100;   // number upto you want to print prime numbers
+
+        // print prime numbers
+       for(int i=1; i<=n; i++){
+        if(isPrime(i)) System.out.print(i + " ");
+       }
+    }
+
+    // function to check weather the given number is prime or not
+    static boolean isPrime(int n){
+        if(n<2) return false;
+        int i=2;
+        while((i*i)<= n){
+            if(n%i == 0) return false;
+            i++;
         }
+        return i*i > n;
     }
 }
